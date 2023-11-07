b416e9d3f2a2f17bdf370eb0d0f11d1366dde662
Bui Tuan <147406431+buituandev@users.noreply.github.com>
2023-11-07 17:58:32 +0700

    Edited

---
+++ src/main/java/eiu/cit/netprog/LowPortScanner.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eiu.cit.netprog;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ */
+public class LowPortScanner {
+
+    public static void main(String[] args) {
+        String host = args.length > 0 ? args[0] : "localhost";
+        for (int i = 0; i < 1024; i++) {
+            try {
+                Socket s = new Socket(host, i);
+                System.out.println("There is a server on port " + i + " of " + host);
+                s.close();
+            } catch (UnknownHostException ex) {
+                System.err.println(ex);
+                break;
+            } catch (IOException ex) {
+            }
+        }
+    }
+}
